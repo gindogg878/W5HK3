351dc6b18f57861e3574be93cf6488e11986ab62
Gino Torres Jr <gindogg878@icloud.com>
2023-03-15 23:31:12 -0500

    finished part 1-3

--- script.js
+++ script.js
@@ -4,45 +4,40 @@ let quotes = [
   `You can have any brew you want... as long as it's a Corona.`,
   `You almost had me? You never had me - you never had your car!`,
   `I don't have friends. I have family.`,
-  `It don't matter if you win by an inch or a mile. Winning's winning.`
+  `It don't matter if you win by an inch or a mile. Winning's winning.`,
 ];
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   // Random quote of the day generator
-  const randomQuote = function() {
-    document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+  const randomQuote = function () {
+    document.querySelector("#quote-of-the-day").textContent = `"${
+      quotes[Math.floor(Math.random() * quotes.length)]
+    }"`;
   };
   randomQuote();
-  
-  // Do all of your work inside the document.addEventListener  
+
+  // Do all of your work inside the document.addEventListener
 
   // Part 1
-
+  const newTitle = document.querySelector("#main-title");
+  // console.log(newTitle);
+  newTitle.textContent = "Dom Torreto & Family's Page";
 
   // Part 2
-
-
+  const body = document.querySelector("body");
+  body.style.backgroundColor = "#F2CD5C";
   // Part 3
-
-
+  const faves = document.querySelector("#favorite-things");
+  faves.removeChild(faves.lastElementChild);
   // Part 4
-
 
   // Part 5
 
-
   // Part 6
-
 
   // Part 7
 
-
   // Part 8
 
-
   // Part 9
-
-
-
-
 });
